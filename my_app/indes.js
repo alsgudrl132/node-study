502bfbed26efabc18b6a91723fe40e343dc78182
alsgudrl132 <alsgudrl132@gmail.com>
2025-03-19 12:22:04 +0900

    서버에서 파일전송하기(HTML)

--- my_app/indes.js
+++ my_app/indes.js
@@ -1,6 +1,16 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
 // middleware
-app.listen(3000);
+app.get("/", (req, res) => {
+  // res.send("Main page GET request");
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
